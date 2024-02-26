94ddd0d351cb563c75d058b17192f8ce6a190896
sedemhaligah <79118259+sedemhaligah@users.noreply.github.com>
2024-02-26 17:06:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const pic = document.querySelector('img')
+const pic = document.querySelector('img');
+const picConatainer = document.querySelector('.picContainer');
 let isZoomed =false;
 
-pic.addEventListener("touchstart", function(){
+picContainer.addEventListener("touchstart", function(){
     if(isZoomed) {
         pic.classList.add("zoomed");
         isZoomed = true;
     }
 });
 
-pic.addEventListener("touchend", function(e){
+picContainer.addEventListener("touchend", function(e){
     if(!pic.contains(e.target)) {
         pic.classList.remove("zoomed");
         isZoomed = false;
